5336ef958e21b04b0fa1d25cf3f0b40d77b58e57
Ashley Johnson <asjohnso@microsoft.com>
2022-09-22 20:01:04 -0700

    Divided into project folders

---
+++ hardcoded-clean/README.md
@@ -0,0 +1,27 @@
+# Learn URL Appender
+
+A Google Chrome extension that allows you to copy a Microsoft Learn page URL from the context menu or on click on the extension icon, and automatically add your desired tracking parameter for sharing with others.
+
+## Permissions
+
+* **activeTab** - allows the extension to obtain the URL of the currently viewed web page after click on the extension icon
+
+* **clipboardWrite** - allows the extension to put the URL into clipboard
+
+* **contextMenus** - allows the extension to add 'Copy Page URL' and 'Copy Frame URL' items to the context menu
+
+* **storage** - allows the extension to store extension settings in the browser storage
+
+## Changelog
+
+[Changelog](CHANGELOG.md)
+
+## Privacy Policy
+
+[Privacy Policy](PRIVACY_POLICY.md)
+
+## License
+
+Code: [MIT](LICENSE)
+
+Icons: [Public Domain Copy / Clone Icon](http://openclipart.org/detail/169987/copy--clone-icon-by-ben)
---
+++ mvp-manifest-v3/background.js
@@ -0,0 +1,43 @@
+// default state goes here
+// this runs ONE TIME ONLY (unless the user reinstalls your extension)
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+      chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          files: ["./foreground.js"]
+      })
+          .then(() => {
+              console.log("INJECTED THE FOREGROUND SCRIPT.");
+          })
+          .catch(err => console.log(err));
+  }
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    'id': 'copy-page-url',
+    'title': 'Copy URL and add tracking',
+    'contexts':[
+      'page',
+      'selection',
+      'link',
+      'editable',
+      'image',
+      'video',
+      'audio'
+    ]
+  });
+
+  chrome.contextMenus.create({
+    'id': 'copy-frame-url',
+    'title': 'Copy Frame URL',
+    'contexts':[
+      'frame'
+    ]
+  });
+
+  chrome.browserAction.setIcon({
+    path: 'icon/LearnAppenderIcon128.png'
+   });
+   
+});
